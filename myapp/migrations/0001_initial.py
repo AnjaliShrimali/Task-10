9aa318050e75fe5c712865231c67df0b372e8cde
Anjali <anjalishrimali6@gmail.com>
2021-06-14 11:34:00 +0530

    Task 10 uploaded

---
+++ myapp/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Driver',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('first_name', models.CharField(max_length=30)),
+                ('last_name', models.CharField(max_length=30)),
+                ('team_name', models.CharField(max_length=30)),
+                ('driver_number', models.IntegerField()),
+                ('nationality', models.CharField(max_length=30)),
+            ],
+        ),
+    ]
